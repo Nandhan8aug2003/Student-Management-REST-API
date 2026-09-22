9633b299ec1e33246d3bc63064a5c8896cc9834e
Nandhan8aug2003 <nandhan8aug2003@gmail.com>
2026-09-22 23:15:54 +0530

    Updated...

--- src/main/java/com/StuMang/REST/API/RestApiApplication.java
+++ src/main/java/com/StuMang/REST/API/RestApiApplication.java
@@ -2,11 +2,13 @@ package com.StuMang.REST.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RestApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
