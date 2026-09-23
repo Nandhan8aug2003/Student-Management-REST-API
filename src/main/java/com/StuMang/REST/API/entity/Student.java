c128ff04f0cfe4992b366eacda8c4fd556f64697
Nandhan8aug2003 <nandhan8aug2003@gmail.com>
2026-09-23 16:12:51 +0530

    created new class student entity

---
+++ src/main/java/com/StuMang/REST/API/entity/Student.java
@@ -0,0 +1,49 @@
+package com.StuMang.REST.API.entity;
+
+public class Student {
+    private String name;
+    private String email;
+    private int age;
+    private int rollno;
+    private String subject;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
