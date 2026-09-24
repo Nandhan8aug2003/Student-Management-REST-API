1c8d315b94f808e9a0a8dfb7219baace1006ac72
Nandhan8aug2003 <nandhan8aug2003@gmail.com>
2026-09-24 22:43:04 +0530

    mapping the post method

--- src/main/java/com/StuMang/REST/API/controller/StudentController.java
+++ src/main/java/com/StuMang/REST/API/controller/StudentController.java
@@ -1,11 +1,24 @@
 package com.StuMang.REST.API.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/student")
+@RequestMapping("/api/students")
 public class StudentController {
+
+    //create
+    @PostMapping("/create")
+    public void createStudent(){
+
+    }
+
+    //read
+
+    //update
+
+    //delete
 
 
 }
