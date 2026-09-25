28cc347f31da21908f3725a8768ff54dd7d53ad4
Nandhan8aug2003 <nandhan8aug2003@gmail.com>
2026-09-25 16:18:04 +0530

    StudentRepository class changed to interface to use jpa methods.

--- src/main/java/com/StuMang/REST/API/repository/StudentRepository.java
+++ src/main/java/com/StuMang/REST/API/repository/StudentRepository.java
@@ -1,12 +1,9 @@
 package com.StuMang.REST.API.repository;
 
 import com.StuMang.REST.API.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StudentRepository {
-    public Student saveStudent(Student stuCreReq){
-        System.out.println("inside controller");
-        return stuCreReq;
-    }
+public interface StudentRepository extends JpaRepository<Student,Long> {
 }
